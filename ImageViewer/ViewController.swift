31170c30c8e5d8824f88010e13b9dfa9f3968a5d
Craig Grummitt <craiggrummitt@gmail.com>
2017-03-15 15:57:38 -0500

    Switched swipes to taps

--- ImageViewer/ViewController.swift
+++ ImageViewer/ViewController.swift
@@ -9,56 +9,59 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var imageView: UIImageView!
+  
+  @IBOutlet weak var imageView: UIImageView!
+  
+  let images = ["CradleMountain.JPG","Laguna69.JPG","PatagoniaSky.JPG"]
+  var imageNo = 0
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    let tapGestureRecognizer =
+      UITapGestureRecognizer(target: self, action: #selector(handleTap))
     
-    let images = ["CradleMountain.JPG","Laguna69.JPG","PatagoniaSky.JPG"]
-    var imageNo = 0
+    tapGestureRecognizer.numberOfTouchesRequired = 1
+    tapGestureRecognizer.numberOfTapsRequired = 2
+    imageView.addGestureRecognizer(tapGestureRecognizer)
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        let swipeLeftGestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe))
-        swipeLeftGestureRecognizer.numberOfTouchesRequired = 3
-        swipeLeftGestureRecognizer.direction = .left
-        imageView.addGestureRecognizer(swipeLeftGestureRecognizer)
-        
-        let swipeRightGestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe))
-        swipeRightGestureRecognizer.numberOfTouchesRequired = 3
-        swipeRightGestureRecognizer.direction = .right
-        imageView.addGestureRecognizer(swipeRightGestureRecognizer)
+    let tapGestureRecognizer2 =
+      UITapGestureRecognizer(target: self, action: #selector(handleTap))
+    
+    tapGestureRecognizer2.numberOfTouchesRequired = 2
+    tapGestureRecognizer2.numberOfTapsRequired = 2
+    imageView.addGestureRecognizer(tapGestureRecognizer2)
+  }
+  @IBAction func handlePan(_ sender: UIPanGestureRecognizer) {
+    let translation = sender.translation(in: self.view)
+    imageView.center = CGPoint(x:imageView.center.x + translation.x,y:imageView.center.y + translation.y)
+    sender.setTranslation(CGPoint.zero, in: self.view)
+  }
+  @IBAction func handlePinch(_ sender: UIPinchGestureRecognizer) {
+    imageView.transform = imageView.transform.scaledBy(x: sender.scale, y: sender.scale)
+    sender.scale = 1
+  }
+  @IBAction func handleRotation(_ sender: UIRotationGestureRecognizer) {
+    imageView.transform = imageView.transform.rotated(by: sender.rotation)
+    sender.rotation = 0
+  }
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  func handleTap(_ sender: UITapGestureRecognizer) {
+    if (sender.numberOfTouchesRequired == 1) {
+      imageNo += 1
+      if imageNo == images.count {imageNo = 0}
+    } else if sender.numberOfTouchesRequired == 2 {
+      imageNo -= 1
+      if imageNo == -1 {imageNo = images.count - 1}
     }
-    @IBAction func handlePan(_ sender: UIPanGestureRecognizer) {
-        let translation = sender.translation(in: self.view)
-        imageView.center = CGPoint(x:imageView.center.x + translation.x,y:imageView.center.y + translation.y)
-        sender.setTranslation(CGPoint.zero, in: self.view)
-    }
-    @IBAction func handlePinch(_ sender: UIPinchGestureRecognizer) {
-        imageView.transform = imageView.transform.scaledBy(x: sender.scale, y: sender.scale)
-        sender.scale = 1
-    }
-    @IBAction func handleRotation(_ sender: UIRotationGestureRecognizer) {
-        imageView.transform = imageView.transform.rotated(by: sender.rotation)
-        sender.rotation = 0
-    }
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    func handleSwipe(_ sender: UISwipeGestureRecognizer) {
-        if sender.direction == .left {
-            imageNo += 1
-            if imageNo == images.count {imageNo = 0}
-        } else if sender.direction == .right {
-            imageNo -= 1
-            if imageNo == -1 {imageNo = images.count - 1}
-        }
-        imageView.image = UIImage(named: images[imageNo])
-    }
-
+    imageView.image = UIImage(named: images[imageNo])
+  }
+  
 }
 extension ViewController:UIGestureRecognizerDelegate {
-    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
-        return true
-    }
+  func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+    return true
+  }
 }
